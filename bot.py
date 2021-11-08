3834962b8d5f8fcc901d6f76d41eff0f8dcd214d
Johann Mendoza <johannmendoza370@gmail.com>
2021-11-08 15:41:11 -0600

    Created command to pick a random game from steam

--- bot.py
+++ bot.py
@@ -3,12 +3,23 @@ import discord
 import random
 from discord.ext import commands
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
+
+steamids = {
+    "JohannM": "76561198142616412",
+    "MarcccOlives": "76561198404538375",
+    "lightfriend7": "76561198236337610",
+    "RedJasper": "76561198839766543",
+    "Demon": "76561198167845439",
+    "vegito523": "76561198142570382",
+    "BOOMER": "76561199112017560"
+}
 
 
 @bot.event
@@ -25,6 +36,27 @@ async def get_game(ctx):
 
     response = random.choice(games)
     await ctx.send(response, delete_after=5)
+
+
+@bot.command(name='getsteamgame')
+async def get_steam_game(ctx):
+    parameters = {
+        "key": os.getenv('STEAM_KEY'),
+        "steamid": steamids[ctx.message.author.name],
+        "format": "json",
+        "include_appinfo": True,
+        "include_played_free_games": True
+    }
+    url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001"
+
+    response = requests.get(url, params=parameters).json()['response']
+
+    games = []
+
+    for game in response['games']:
+        games.append(game["name"])
+
+    await ctx.send(random.choice(games), delete_after=10)
 
 
 @bot.command(name='addgame')
